d07f92497e59f37dc46b7c8cdc6ec9ff146fc8ae
Zaun14 <zaunmasters@gmail.com>
2023-08-11 10:52:21 +0300

    Refactoring

--- Services/Clicker/Clicker.cs
+++ Services/Clicker/Clicker.cs
@@ -1,4 +1,4 @@
-﻿using Arphox.MouseManipulator;
+﻿using AutoClicker.Extensions;
 using WindowsInput;
 using WindowsInput.Native;
 
@@ -11,44 +11,21 @@ namespace AutoClicker.Services.Clicker
             InputSimulator input = new();
 
             if (IsKeyboardKey(key))
-            {
                 input.Keyboard.KeyPress((VirtualKeyCode)key);
-            }
-            if ((int)MouseButtons.Right == key)
-            {
-                MouseManipulator.RightClick();
-            }
-            else if ((int)MouseButtons.Left == key)
-            {
-                MouseManipulator.LeftClick();
-            }
-            else if ((int)MouseButtons.Middle == key)
-            {
-                MouseManipulator.MiddleClick();
-            }
-            else if ((int)MouseButtons.XButton1 == key)
-            {
-                input.Mouse.XButtonClick(1);
-            }
-            else if ((int)MouseButtons.XButton1 == key)
-            {
-                input.Mouse.XButtonClick(2);
-            }
+            else
+                input.ClickByIntkey(key);
 
-            Thread.Sleep(10);
+            Thread.Sleep(1);
         }
-
 
         private static bool IsKeyboardKey(int key)
         {
-            if (key != 0x00100000 &&
+            return
+                key != 0x00100000 &&
                 key != 0x00200000 &&
                 key != 0x00400000 &&
                 key != 0x00800000 &&
-                key != 0x01000000)
-                return true;
-            else
-                return false;
+                key != 0x01000000;
         }
     }
 }
